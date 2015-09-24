a84d22d8a0c02bb1dbc3d6277be9245f7a6595e0
yliu84 <yliu84@studentmail.nait.ca>
2015-09-23 20:46:44 -0600

    add AdminController and eRestaurantContext

---
+++ eRestaurantDemo/eRestaurantSystem/BLL/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using eRestaurantSystem.DAL;
+using eRestaurantSystem.DAL.Entities;
+using System.ComponentModel; // Object Data Source
+#endregion
+
+namespace eRestaurantSystem.BLL
+{
+    [DataObject]
+    public class AdminController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<SpecialEvent> SpecialEvents_List()
+        {
+            //connect to our DbContext class in the DAL
+            //create an instance of the class
+            //we will use a transaction to hold our query
+            using (var context = new eRestaurantContext())
+            {
+                //method syntax
+                return context.SpecialEvents.OrderBy(x => x.Description).ToList();
+            }
+        }
+    }
+}
---
+++ eRestaurantDemo/eRestaurantSystem/DAL/eRestaurantContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using eRestaurantSystem.DAL.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+#endregion
+
+namespace eRestaurantSystem.DAL
+{   
+    //this class should only be accessiable from classes inside
+    //this component library
+    //therefore the access level for this class will be internal
+
+    //this class will inherit from DBContext (EntityFramework)
+    public class eRestaurantContext : DbContext
+    {
+        //create a constructor which will pass the connection string
+        //name to the DBContext
+        public eRestaurantContext() : base("name=EatIn")
+        {
+
+        }
+
+        //set up of mapping DbSet<T> property
+        //map an entity to a database table
+        public DbSet<SpecialEvent> SpecialEvents { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Table> Tables { get; set; }
+        
+        //when overriding the OnModelCreating(), it is important
+        //to remember to call the base method's implementation
+        //before you exit the method
+
+        //the ManyToManyNavigationPropertyConfiguration.Map method
+        //lets you configure the tables and colums used for this
+        //many to many relationship.
+
+        //it takes a ManyToManyNavigationPropertyConfiguration instance
+        //in which you specify the column names by calling the MapleftKey,
+        //MapRightKey, and ToTable methods
+
+        //the "left" key is the one specified in the HasMany method
+        //the "right" key is the one specified in the WithMany method
+
+        //this navigation replaces the sql associated tabel that breaks
+        //up a many to many relationship
+        //this technique should ONLY be used if the associate table in
+        //sql has ONLY a compound primay key and NO non-key attributes
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Reservation>().HasMany(r => r.Tables)
+                .WithMany(t => t.Reservations)
+                .Map(mapping =>
+                {
+                    mapping.ToTable("ReservationTables");
+                    mapping.MapLeftKey("ReservationID");
+                    mapping.MapRightKey("TableID");
+                });
+            base.OnModelCreating(modelBuilder); //DO NOT REMOVE
+        }
+    }
+}
