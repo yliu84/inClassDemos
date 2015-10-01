3698f1b6956a795fc9d0f3c2cbeb34da5c706d7a
yliu84 <yliu84@studentmail.nait.ca>
2015-09-30 20:26:40 -0600

    add other entities in eRestaurant

--- eRestaurantDemo/eRestaurantSystem/DAL/eRestaurantContext.cs
+++ eRestaurantDemo/eRestaurantSystem/DAL/eRestaurantContext.cs
@@ -31,7 +31,12 @@ namespace eRestaurantSystem.DAL
         public DbSet<SpecialEvent> SpecialEvents { get; set; }
         public DbSet<Reservation> Reservations { get; set; }
         public DbSet<Table> Tables { get; set; }
-        
+
+        public DbSet<Bill> Bills { get; set; }
+        public DbSet<BillItem> BillItems { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<MenuCategory> MenuCategories { get; set; }
+        public DbSet<Waiter> Waiters { get; set; }
         //when overriding the OnModelCreating(), it is important
         //to remember to call the base method's implementation
         //before you exit the method
