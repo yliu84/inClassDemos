cf91a56e9534fe6fa3b8ad5090dbc3b23e3ca932
yliu84 <yliu84@studentmail.nait.ca>
2015-11-25 20:33:39 -0700

    Role manager set up

---
+++ eRestaurantDemo/eRestaurantSystem/BLL/Security/RoleManager.cs
@@ -0,0 +1,32 @@
+﻿using eRestaurantSystem.DAL.Entities.Security;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurantSystem.BLL.Security
+{
+    public class RoleManager:RoleManager<IdentityRole>
+    {
+        public RoleManager()
+            : base(new RoleStore<IdentityRole>(new ApplicationDbContext()))
+        {
+
+        }
+
+        public void AddDefaultRoles()
+        {
+            foreach (string roleName in SecurityRoles.DefaultSecurityRoles)
+            {
+                // Check if it exists
+                if (!Roles.Any(r => r.Name == roleName))
+                {
+                    this.Create(new IdentityRole(roleName));
+                }
+            }
+        }
+    }
+}
